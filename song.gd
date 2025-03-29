551df035586b3c2ae9253959915fd3e8d373a3bd
FabsPros <prosfabijan@gmail.com>
2025-03-29 23:48:39 +0100

    peeinmymouth

--- song.gd
+++ song.gd
@@ -6,9 +6,11 @@ extends Control
 @onready var secondLine = $MarginContainer/lines/MarginContainer2/secondLine
 @onready var lines = $MarginContainer/lines
 @onready var changeLineTimer = $changeLineTimer
+@onready var audio = $AudioStreamPlayer
 
 var text = ""
 var lineNumb = 0
+var wait_times = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,6 +19,8 @@ func _ready() -> void:
 	if Global.current_song == 0:
 		img_texture = load("res://qrcodes/placeholder.png")
 		file =  FileAccess.open("res://lyrics/callmemaybe.txt", FileAccess.READ)
+		audio.stream = load("res://songs/callmemaybe.mp3")
+		wait_times = Global.callmemaybetimes
 		
 	qrcode.texture = img_texture
 	text = file.get_as_text()
@@ -27,8 +31,10 @@ func _ready() -> void:
 func players_joined():
 	code_label.visible = false
 	qrcode.visible = false
+	changeLineTimer.wait_time = wait_times[0]
 	changeLineTimer.start()
 	lines.visible = true
+	audio.play()
 	#print(text)
 
 
@@ -38,11 +44,6 @@ func _on_change_line_timer_timeout() -> void: # USE REPLACE TO REPLACE
 	line = line.replace("._________", ("_"+"WORD2"+"_"))
 	firstLine.text = line
 	lineNumb += 1 
-	line = str(text.get_slice("\r\n", lineNumb))
-	line = line.replace("__________", ("_"+"WORD"+"_"))
-	line = line.replace("._________", ("_"+"WORD2"+"_"))
-	secondLine.text = line
-	lineNumb += 1
-	#text.remove_at(0)
-	# SET NEW TIMER TIME AND NEW WORD AND WORD2
-	changeLineTimer.start()
+	if lineNumb < wait_times.size():
+		changeLineTimer.wait_time = wait_times[lineNumb]
+		changeLineTimer.start()
